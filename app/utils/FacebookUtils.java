2ee89c20c8b610c45bc0f6f53239a11696d61956
Anand <anand@heroku.com>
2012-02-10 19:10:05 -0800

    removed picture parameter in FacebookUtils

--- app/utils/FacebookUtils.java
+++ app/utils/FacebookUtils.java
@@ -13,7 +13,7 @@ public class FacebookUtils {
 		HashMap<String,Object> params = new HashMap<String,Object>();
 		params.put("access_token",System.getenv("FB_ACCESS_TOKEN"));
 		params.put("link", URLEncoder.encode(String.format("http://barcodegen.herokuapp.com/coupon?couponId=%s",coupon.couponId),"UTF-8"));
-		params.put("picture", URLEncoder.encode(String.format("http://barcodegen.herokuapp.com/public/images/favicon.png"),"UTF-8"));
+		//params.put("picture", URLEncoder.encode(String.format("http://barcodegen.herokuapp.com/public/images/favicon.png"),"UTF-8"));
 		WS.url("https://graph.facebook.com/me/links").params(params).post();
 		
 	}
