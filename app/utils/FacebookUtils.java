8f9c80cc930e0928703ec3a60fe85cb305e2b1ff
Anand <anand@heroku.com>
2012-02-11 09:53:23 -0800

    reversed the order of refresh and post
    /

--- app/utils/FacebookUtils.java
+++ app/utils/FacebookUtils.java
@@ -11,14 +11,14 @@ public class FacebookUtils {
 
 	public static void shareCouponOnFacebook(DiscountCoupon coupon) throws UnsupportedEncodingException{
 		HashMap<String,Object> params = new HashMap<String,Object>();
-		params.put("access_token",System.getenv("FB_ACCESS_TOKEN"));
-		params.put("link", URLEncoder.encode(String.format("http://barcodegen.herokuapp.com/coupon?couponId=%s",coupon.couponId),"UTF-8"));
-		//params.put("picture", URLEncoder.encode(String.format("http://barcodegen.herokuapp.com/public/images/favicon.png"),"UTF-8"));
-		WS.url("https://graph.facebook.com/me/links").params(params).post();
-		params = new HashMap<String,Object>();
 		params.put("q",URLEncoder.encode(String.format("http://barcodegen.herokuapp.com/coupon?couponId=%s",coupon.couponId),"UTF-8"));
 		params.put("fbrefresh","true");
 		WS.url("http://developers.facebook.com/tools/debug/og/object").params(params).get();	
 		System.out.println(">>>>>>>>>>>Refreshed thumbnail link");
+		params = new HashMap<String,Object>();
+		params.put("access_token",System.getenv("FB_ACCESS_TOKEN"));
+		params.put("link", URLEncoder.encode(String.format("http://barcodegen.herokuapp.com/coupon?couponId=%s",coupon.couponId),"UTF-8"));
+		//params.put("picture", URLEncoder.encode(String.format("http://barcodegen.herokuapp.com/public/images/favicon.png"),"UTF-8"));
+		WS.url("https://graph.facebook.com/me/links").params(params).post();
 	}
 }
