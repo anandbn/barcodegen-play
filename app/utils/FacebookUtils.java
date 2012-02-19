3abef3a80a2d349365876f2108e967eae3dcfa66
Anand <anand@heroku.com>
2012-02-19 10:46:38 -0800

    added file upload

--- app/utils/FacebookUtils.java
+++ app/utils/FacebookUtils.java
@@ -1,10 +1,19 @@
 package utils;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.util.HashMap;
+import java.util.UUID;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
 
 import play.libs.WS;
+import play.libs.WS.FileParam;
+import play.mvc.Http.Response;
 import models.DiscountCoupon;
 
 public class FacebookUtils {
@@ -18,6 +27,23 @@ public class FacebookUtils {
 																							coupon.couponId)
 																	,"UTF-8"));
 			params.put("message","Coupon:"+coupon.title);
-			WS.url("https://graph.facebook.com/me/links").params(params).post();
+			File tmpImg = new File(System.getProperty("java.io.tmpdir") + File.separator + coupon.id+".png");
+			try{
+				FileOutputStream fileOut = new FileOutputStream(tmpImg);
+				System.out.println(">>>>>>>>>>>>Img file Path:"+tmpImg.getAbsolutePath()+tmpImg.getName());
+				QRCodeGenerator.generateCodeToStream(fileOut,coupon.couponText,
+						BarcodeFormat.valueOf(coupon.codeType),coupon.height,coupon.width,coupon.contentType);
+				fileOut.close();
+				System.out.println(">>>>>>>>>>>>Completed writing image to file: "+tmpImg.getAbsolutePath()+tmpImg.getName());
+			}catch(IOException ex){
+				ex.printStackTrace();
+			} catch (WriterException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			WS.FileParam imageFile = new WS.FileParam(tmpImg, "data");
+			WS.HttpResponse response  = WS.url("https://graph.facebook.com/100003142167612/photos").params(params).files(imageFile).timeout("5mn").post();
+			System.out.println(String.format("Posted Image to /photos. Response code:%s,Body:%s",response.getStatus(),response.getJson().toString()));
+			tmpImg.delete();
 		}
 }
